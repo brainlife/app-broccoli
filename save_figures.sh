4d38b9d5cf1b7a41c6d8a9a40db99c4209ff222c
Giulia Berto <giulia.berto.4@gmail.com>
2021-05-18 09:04:14 -0400

    update

--- save_figures.sh
+++ save_figures.sh
@@ -5,9 +5,9 @@ set -x
 outdir=figures
 mkdir -p $outdir
 
-map=output/sub-${sub}/activation_map_MNI.nii.gz
-t1=output/sub-${sub}/$taskname/sub-${sub}_t1_aligned_mni_nonlinear.nii
-epi=output/sub-${sub}/$taskname/sub-${sub}_epi_aligned_mni_nonlinear.nii
+map=$1
+t1=$2
+epi=$3
 
 # render overlay t1 and map
 fsleyes render -ds "world" -hc -hl -c 2 -ad -xz 500 -yz 500 -zz 500 -vl 45 52 45 --outfile ${outdir}/t1_map_mni.png $t1 $map -ot volume -a 75 --cmap red-yellow
