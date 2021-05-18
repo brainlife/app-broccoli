dc93fba411dca7960d8f37ac2216c4cb9065217b
Giulia Berto <giulia.berto.4@gmail.com>
2021-05-18 11:19:11 -0400

    added bet

--- save_figures.sh
+++ save_figures.sh
@@ -14,4 +14,4 @@ mask=$4
 fsleyes render -ds "world" -hc -hl -c 2 -ad -xz 500 -yz 500 -zz 500 -vl 45 52 45 --outfile ${outdir}/t1_map_mni.png $t1 $map -ot volume -a 75 --cmap red-yellow -un $mask -ot mask -o -mc 1 1 0
 
 # render overlay epi and map
-fsleyes render -ds "world" -hc -hl -c 2 -ad -xz 500 -yz 500 -zz 500 -vl 45 52 45 --outfile ${outdir}/epi_map_mni.png $epi -b 40 $map -ot volume -a 75 --cmap red-yellow -un $mask -ot mask -o -mc 1 1 0
+fsleyes render -ds "world" -hc -hl -c 2 -ad -xz 500 -yz 500 -zz 500 -vl 45 52 45 --outfile ${outdir}/epi_map_mni.png $epi -b 40 $map -ot volume -a 75 --cmap red-yellow -un $mask 
